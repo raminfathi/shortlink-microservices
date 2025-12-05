a2514963289497db549de04e89bd81922631a933
ramin <f.ramin2017@gmail.com>
2025-12-05 20:44:45 +0330

    fix bux afrer add nginx

--- core-api/app/config.py
+++ core-api/app/config.py
@@ -4,21 +4,15 @@ import logging
 
 class Settings(BaseSettings):
     REDIS_HOST: str = "redis-stack"
-    REDIS_PORT: int = 6379  # <--- ۱. پورت ردیس را اضافه می‌کنیم (برای هماهنگی)
+    REDIS_PORT: int = 6379
+    BASE_URL: str = "http://localhost"
 
-    BASE_URL: str = "http://localhost:8000"  # <--- ۲. آدرس پایه را به تنظیمات منتقل می‌کنیم
-
-    # نام استریم
     QR_CODE_JOBS_STREAM: str = "qr_code_jobs"
 
-    # VVV --- ۳. این دو خط جدید هستند --- VVV
-    # پیشوند کلید Hash برای داده‌های لینک (QR, آمار)
     DATA_HASH_KEY_PREFIX: str = "data"
     ANALYTICS_STREAM_NAME: str = "analytics_jobs"
 
-    # مسیری که فایل‌های استاتیک (QR Code) در آن قرار دارند
     MEDIA_PATH: str = "/app/media"
-    # ^^^ --- پایان بخش جدید --- ^^^
 
 
 settings = Settings()
