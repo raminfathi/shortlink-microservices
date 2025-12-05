0a52ccbe07861d0bf18fe7d04125f3c124c6433f
ramin <f.ramin2017@gmail.com>
2025-12-05 15:30:34 +0330

    Add monitoring stack (Prometheus, Grafana, cAdvisor), Integration Tests, and Leaderboard features

--- core-api/app/main.py
+++ core-api/app/main.py
@@ -1,36 +1,41 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles  # <-- ۱. این import جدید است
+from fastapi.staticfiles import StaticFiles
+# VVV --- 1. Import --- VVV
+from prometheus_fastapi_instrumentator import Instrumentator
+# ^^^ --- End Import --- ^^^
 from .database import redis_client
 from .routers import links
-from .config import settings  # <-- ۲. این import جدید است
+from .config import settings
 
-# ایجاد اپلیکیشن FastAPI
 app = FastAPI(
     title="ShortLink Core API",
-    description="سرویس اصلی برای ساخت و ریدایرکت لینک‌های کوتاه",
+    description="URL Shortener API with Monitoring",
     version="1.0.0"
 )
 
-# --- رویدادهای Startup و Shutdown ---
+# VVV --- 2. Activate Monitoring  --- VVV
+# This creates the /metrics endpoint
+instrumentator = Instrumentator().instrument(app)
+instrumentator.expose(app)
+
+
+# ^^^ --- End Monitoring --- ^^^
+
+
+# --- Startup & Shutdown ---
 @app.on_event("startup")
 async def startup_app():
-    # اتصال به ردیس
     await redis_client.connect()
+
 
 @app.on_event("shutdown")
 async def shutdown_app():
     await redis_client.disconnect()
 
-# --- روت‌ها ---
-# اضافه کردن روتر لینک‌ها (مانند /links و /{short_id})
-app.include_router(links.router)
 
-# VVV --- ۳. این بخش جدید است --- VVV
-# سرویس‌دهی فایل‌های استاتیک
-# این به FastAPI می‌گوید که هر درخواستی به '/media' را
-# به فایل‌های داخل پوشه 'settings.MEDIA_PATH' (یعنی /app/media) مپ کن.
+# --- Routers & Mounts ---
+app.include_router(links.router)
 app.mount("/media", StaticFiles(directory=settings.MEDIA_PATH), name="media")
-# ^^^ --- پایان بخش جدید --- ^^^
 
 
 @app.get("/")
